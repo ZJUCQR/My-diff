27957729942db4b95a697cdcee1c5064b63f9952
ZJUCQR <310857001@qq.com>
2026-02-28 03:34:18 +0000

    add train edit

---
+++ examples/z_image/model_training/lora/Z-Image-Turbo-Edit-4.sh
@@ -0,0 +1,18 @@
+# Z-Image-Turbo image editing LoRA training on 4*A100
+accelerate launch --num_processes 4 examples/z_image/model_training/train.py \
+  --dataset_base_path data/example_image_edit_dataset \
+  --dataset_metadata_path data/example_image_edit_dataset/metadata_edit.json \
+  --data_file_keys "image,edit_image" \
+  --extra_inputs "edit_image" \
+  --max_pixels 1048576 \
+  --dataset_repeat 1 \
+  --model_id_with_origin_paths "Tongyi-MAI/Z-Image-Turbo:transformer/*.safetensors,Tongyi-MAI/Z-Image-Turbo:text_encoder/*.safetensors,Tongyi-MAI/Z-Image-Turbo:vae/diffusion_pytorch_model.safetensors" \
+  --learning_rate 1e-4 \
+  --num_epochs 5 \
+  --remove_prefix_in_ckpt "pipe.dit." \
+  --output_path "./models/train/Z-Image-Turbo_lora_edit" \
+  --lora_base_model "dit" \
+  --lora_target_modules "to_q,to_k,to_v,to_out.0,w1,w2,w3" \
+  --lora_rank 32 \
+  --use_gradient_checkpointing \
+  --dataset_num_workers 8
